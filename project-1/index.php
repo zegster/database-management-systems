581f0bb77e9671b2aee13e1f1172f535b923cd62
zegster <dngo1996@gmail.com>
2020-02-18 23:13:04 -0600

    update

--- project-1/index.php
+++ project-1/index.php
@@ -53,7 +53,6 @@ if(isset($_POST["keyword_search"]))
     else
     {
         $sortedResult = mysqli_query($con, "SELECT * FROM $table_name WHERE $searchQuery ORDER BY $fields[$cn]");
-        echo "SELECT * FROM $table_name WHERE $searchQuery ORDER BY $fields[$cn] DESC";
     }
 }
 else
